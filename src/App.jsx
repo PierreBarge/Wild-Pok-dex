ab69d543a26aaf9977dcd31e16a1aeef3c0f6c2f
PierreBarge <pierrebarge@hotmail.fr>
2024-03-28 18:11:48 +0100

    Feature: Added state for pokemon index, and buttons to switch between mons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      {pokemonIndex > 0 ? (
+        <button onClick={handleClickPrevious}>Précédent</button>
+      ) : (
+        <></>
+      )}
+      {pokemonIndex < pokemonList.length - 1 ? (
+        <button onClick={handleClickNext}>Suivant</button>
+      ) : (
+        <></>
+      )}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
@@ -13,6 +34,21 @@ const pokemonList = [
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
